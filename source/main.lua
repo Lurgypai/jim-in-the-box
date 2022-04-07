2ed7512320f806223745d472473aa47ecfe4c6f1
Isaiah Honor <bonkdroid@gmail.com>
2022-04-06 22:56:09 -0700

    Added jim

--- source/main.lua
+++ source/main.lua
@@ -13,6 +13,9 @@ local scared_pos_x = 100
 local scared_pos_y = 100
 local chill_pos_x <const> = 100
 local chill_pos_y <const> = 100
+local lose_x_1 <const> = 50
+local lose_y_1 <const> = 400
+local lose_y_2 <const> = 200
 local target_pos_x = 100
 local target_pos_y = 100
 local body_offset_x <const> = -28
@@ -33,21 +36,27 @@ local crank_center_x <const> = 270
 local crank_center_y <const> = 140
 local box_offset_x <const> = -122
 local box_offset_y <const> = -45
+local jim_target_x <const> = 210
+local jim_target_y <const> = 70
 
 local r_hand_img = nil
 local char_img = nil
 local table_img = nil
 local l_hand_img = nil
 local box_img = nil
+local jim_img = nil
 
 local r_hand_spr
 local l_hand_spr
 local char_spr
 local box_spr
 local table_spr
+local jim_spr
 
 local crank_radius = 25
 local arm_seg_length = 90
+
+local gameover = false
 
 function startup()
     gfx.setColor(gfx.kColorBlack)
@@ -60,12 +69,14 @@ function startup()
     char_img = gfx.image.new("images/char")
     box_img = gfx.image.new("images/box")
     table_img = gfx.image.new("images/table")
+    jim_img = gfx.image.new("images/jim")
 
     r_hand_spr = gfx.sprite.new(r_hand_img)
     l_hand_spr = gfx.sprite.new(l_hand_img)
     char_spr = gfx.sprite.new(char_img)
     box_spr = gfx.sprite.new(box_img)
     table_spr = gfx.sprite.new(table_img)
+    jim_spr = gfx.sprite.new(jim_img)
 
     r_hand_spr:setCenter(0, 0)
     l_hand_spr:setCenter(0, 0)
@@ -95,6 +106,9 @@ function startup()
     char_spr:add()
     box_spr:add()
     table_spr:add()
+    jim_spr:add()
+
+    jim_spr:setVisible(false)
 end
 
 startup()
@@ -114,11 +128,13 @@ function get_circle_intersect(x1, y1, r1, x2, y2, r2)
 end
 
 function update_charge()
-    local crank_change = playdate.getCrankChange()
-    if (crank_change > 0) then
-        crank_change /= math.random(5)
-        charge += crank_change
-        return crank_change
+    if(not gameover) then
+        local crank_change = playdate.getCrankChange()
+        if (crank_change > 0) then
+            crank_change /= math.random(5)
+            charge += crank_change
+            return crank_change
+        end
     end
     return 0
 end
@@ -126,17 +142,22 @@ end
 function update_body(crank_change)
     local target_x
     local target_y
-    if (crank_change > 0) then
-        scared_pos_y += crank_change / 60
-        scared_pos_x -= crank_change / 180
-       
-        target_x = scared_pos_x
-        target_y = scared_pos_y
-    else
-        target_x = chill_pos_x
-        target_y = chill_pos_y
+    if(not gameover) then
+        if (crank_change > 0) then
+            scared_pos_y += crank_change / 60
+            scared_pos_x -= crank_change / 180
+           
+            target_x = scared_pos_x
+            target_y = scared_pos_y
+        else
+            target_x = chill_pos_x
+            target_y = chill_pos_y
+        end
+    else 
+        target_x = lose_x_1
+        target_y = lose_y_1
     end
-    
+
     local x_diff = target_x - body_pos_x
     if(math.abs(x_diff) > 1) then
         body_pos_x += x_diff / 10
@@ -209,14 +230,48 @@ gfx.sprite.setBackgroundDrawingCallback(
             -- draw crank rod
             gfx.setLineWidth(8)
             gfx.drawLine(crank_center_x + 4, crank_center_y, r_hand_x + 4, r_hand_y + 32)
+
+            if(jim_spr:isVisible()) then
+                gfx.drawLine(jim_target_x, 400, jim_spr.x, jim_spr.y)
+            end
             gfx.clearClipRect()
         end
     )
 
+local jim_y_accel = 0
+local jim_y_vel = 0
+
+function update_jim()
+    if(jim_spr:isVisible()) then
+        jim_y_accel = (jim_target_y - jim_spr.y) / 30
+        jim_y_vel += jim_y_accel
+        local friction <const> = 0.4
+        if (jim_y_vel < 0) then
+            jim_y_vel += friction
+        else
+            jim_y_vel -= friction
+        end
+
+        jim_spr:moveBy(0, jim_y_vel)
+        print(jim_y_vel..", "..jim_y_accel)
+    end
+end
+
+local jim_spawned = false
 
 function playdate.update()
     local crank_change = update_charge()
+    if(charge > 4000 + math.random(3000)) then
+        gameover = true
+        if(not jim_spawned) then
+            jim_spawned = true
+
+            jim_spr:setVisible(true)
+            jim_spr:moveTo(jim_target_x, 200)
+        end
+    end
     update_body(crank_change)
+    update_jim()
 
     gfx.clear(gfx.kColorWhite)
     draw_all()
